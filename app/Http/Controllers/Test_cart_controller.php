a302dacf2aa93a34befb88df622cdbd637ca7887
Alexander <92953068+Alex823d@users.noreply.github.com>
2021-11-13 16:14:15 +0400

    Update Test_cart_controller.php
    comments update

--- app/Http/Controllers/Test_cart_controller.php
+++ app/Http/Controllers/Test_cart_controller.php
@@ -52,11 +52,13 @@ class Test_cart_controller extends Controller
             $result['products'][] =  ['product_id' => $item->product_id,'quantity' => $item->quantity,'price' => $item->price];
         }
 
+        //getting all discount grooups with items in this group by product owners in the cart
         $groups = User_product_group::whereIn('user_id',$user_id)->with('items')->get();
 
         $_discount = [];
         $count_in_cart = [];
         $discount_in_group = [];
+        //grouping discount groups by owner and group id
         foreach ($groups as $group){
             $_discount[$group->user_id][$group->group_id] = $group;
             $count_in_cart[$group->group_id] = 0;
@@ -69,14 +71,18 @@ class Test_cart_controller extends Controller
 
         foreach ($cart as $c_item){
 
+            //determining that a discount group exists for the product in the cart
             if(isset($_discount[$c_item->user_id][$c_item->group_id])){
-                $count_in_group = count($_discount[$c_item->user_id][$c_item->group_id]->items);
+                
+                $count_in_group = count($_discount[$c_item->user_id][$c_item->group_id]->items);//count items in this group
 
                 foreach ($_discount[$c_item->user_id][$c_item->group_id]->items as $item){
 
+                    //if product in cart exists in this group and quantity of this product > 1, increment product count in the cart of this group and counting discount for this product 
                     if(($c_item->quantity > 1) && $c_item->product_id == $item->product_id){
 
                         $count_in_cart[$c_item->group_id]++;
+                        //because discount is available only if this product quantity: 2 in the cart
                         $discount_in_group[$c_item->group_id] += $_discount[$c_item->user_id][$c_item->group_id]->discount * $c_item->price / 100 * 2;
 
                     }
@@ -91,6 +97,7 @@ class Test_cart_controller extends Controller
 
         //dd($cart,$_discount,$discount);
 
+        
         $result['discount'] = $total_discount;
         return $result;
     }
